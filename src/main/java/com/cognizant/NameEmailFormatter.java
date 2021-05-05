1e137a04217d24336196e3dbb6a252e0cd1fa9fb
251549 <nalbandf@gmail.com>
2021-05-05 19:14:57 -0400

    Created NameEmailFormatter.java and updated build.gradle

---
+++ src/main/java/com/cognizant/NameEmailFormatter.java
@@ -0,0 +1,9 @@
+package com.cognizant;
+
+public class NameEmailFormatter {
+    public static void main(String[] args) {
+        System.out.print(args[0]);
+        System.out.print(" ");
+        System.out.println("<"+args[1]+">");
+    }
+}
